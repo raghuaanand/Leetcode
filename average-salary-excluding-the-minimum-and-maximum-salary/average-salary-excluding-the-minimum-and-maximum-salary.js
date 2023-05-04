f3acdc6482b61183484b9d399aea59889c631ad3
Raghu Anand <raghuaanand@gmail.com>
2023-05-04 08:07:16 +0530

    Time: 57 ms (61.13%) | Memory: 42.3 MB (16.18%) - LeetSync

--- average-salary-excluding-the-minimum-and-maximum-salary/average-salary-excluding-the-minimum-and-maximum-salary.js
+++ average-salary-excluding-the-minimum-and-maximum-salary/average-salary-excluding-the-minimum-and-maximum-salary.js
@@ -4,18 +4,12 @@
  */
 var average = function(salary) {
     let size = salary.length;
-    let minSal = Math.min(...salary);
-    let maxSal = Math.max(...salary);
     let sum = 0;
-    for(let i = 0; i < size; i++){
+    for (let i = 0; i < size; i++) {
         sum = sum + salary[i];
     }
 
-    let average = (sum - minSal - maxSal)/(size - 2);
-    function trueRound(average, digits){
-        return (Math.round((average*Math.pow(10,digits)).toFixed(digits-1))
-         /Math.pow(10, digits)).toFixed(digits);
-    }
+    let average = (sum - Math.min(...salary) - Math.max(...salary)) / (size - 2);
 
-    return trueRound(average, 5);
+    return average.toFixed(5);
 };
